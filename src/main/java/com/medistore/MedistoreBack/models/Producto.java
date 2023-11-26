ab496694a87a5bbd7c15e7479c5857caa63b6009
Marcos David Mejia Gómez <marcosdavidmg@ufps.edu.co>
2023-11-26 11:41:03 -0500

    Segundo commit Marcos

--- src/main/java/com/medistore/MedistoreBack/models/Producto.java
+++ src/main/java/com/medistore/MedistoreBack/models/Producto.java
@@ -2,17 +2,38 @@ package com.medistore.MedistoreBack.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 @Data
 @Entity
 @Table(name="producto")
-
 
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+    @Column(name = "lote")
+    private String lote;
+    @Column(name = "fechaIngreso")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date fechaIngreso;
+    @Column(name = "fechaVencimiento")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date fechaVencimiento;
+    @Column(name = "precio")
+    private float precio;
+    @Column(name = "formula")
+    private String formula;
+    @Column (name = "dosis")
+    private String dosis;
+    @Column (name = "marca")
+    private String marca;
+    @Column (name = "disponible")
+    private boolean disponible;
 
-
-
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Venta venta;
 }
---
+++ src/main/java/com/medistore/MedistoreBack/models/Venta.java
@@ -0,0 +1,35 @@
+package com.medistore.MedistoreBack.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name ="venta")
+
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name= "producto")
+    private String producto;
+    @Column(name= "cantidad")
+    private String cantidad;
+    @Column(name= "lote")
+    private String lote;
+    @Column(name= "costo")
+    private float costo;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, fetch =  FetchType.EAGER, mappedBy = "producto")
+    private Set<Producto> Productos = new HashSet<>();
+}
