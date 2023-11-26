80374a80c785f45275ca5ac6ab2d6e604fb257eb
Marcos David Mejia Gómez <marcosdavidmg@ufps.edu.co>
2023-11-26 16:50:39 -0500

    creacion de reporte sin funcionar

---
+++ src/main/java/com/medistore/MedistoreBack/dao/ReporteProductoDao.java
@@ -0,0 +1,12 @@
+package com.medistore.MedistoreBack.dao;
+
+import java.util.List;
+import com.medistore.MedistoreBack.models.ReporteProducto;
+import com.medistore.MedistoreBack.models.Usuario;
+
+public interface ReporteProductoDao {
+
+
+    //List<ReporteProducto> ();
+
+}
---
+++ src/main/java/com/medistore/MedistoreBack/daoImp/ReporteProductoDaoImp.java
@@ -0,0 +1,6 @@
+package com.medistore.MedistoreBack.daoImp;
+
+import com.medistore.MedistoreBack.dao.ReporteProductoDao;
+
+public class ReporteProductoDaoImp implements ReporteProductoDao {
+}
